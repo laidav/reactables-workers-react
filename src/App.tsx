fc1a98586f014e23e4d7c3e5ed3db13890c1a767
David Lai <laidav@gmail.com>
2025-01-28 13:21:31 -0500

    add toggle

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { fromWorker } from "./Helpers/fromWorker";
+import { useReactable } from "@reactables/react";
+import { of } from "rxjs";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import { ToggleState, ToggleActions } from "./RxToggle";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  const [state, actions] = useReactable(() =>
+    fromWorker<ToggleState, ToggleActions>(
+      new Worker(
+        new URL("./Rxtoggle.worker.ts", import.meta.url),
+
+        { type: "module" }
+      ),
+      {
+        sources: [of({ type: "toggle" })],
+      }
+    )
+  );
 
   return (
     <>
@@ -18,6 +35,9 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
+        <button onClick={() => actions.toggle()}>
+          Toggle is: {state ? "on" : "off"}
+        </button>
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
@@ -29,7 +49,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
