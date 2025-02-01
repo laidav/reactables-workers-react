3303679eeb76fa4566d10fad3424bb2dd861600d
David Lai <laidav@gmail.com>
2025-01-31 20:58:11 -0500

    update vite

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,11 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    include: ["@reactables/core", "@reactables/web-workers"],
+    include: [
+      "@reactables/core",
+      "@reactables/react",
+      "@reactables/web-workers",
+    ],
   },
   build: {
     commonjsOptions: {
